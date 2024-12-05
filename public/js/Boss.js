71dff4752879bcf1221f1793174abce5bf269f39
jordan-creyelman <creyelmanjo@gmail.com>
2024-12-05 09:06:12 +0100

    add class boss

---
+++ public/js/Boss.js
@@ -0,0 +1,15 @@
+export class Boss{
+    constructor(nom,attaque,vie) {
+        this.nom = nom;
+        this.attaque = attaque
+        this.vie = vie;
+        this.subir = (degats) => {
+            if (this.vie - degats > 0){
+                this.vie -= degats;
+
+            } else {
+                alert("Monstre mort");
+            }
+        }
+    }
+}
