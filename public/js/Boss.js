50b625e6c9ae460efcda6d85c9191e96ebc03f0d
jordan-creyelman <creyelmanjo@gmail.com>
2024-12-06 09:36:55 +0100

    add element

--- public/js/Boss.js
+++ public/js/Boss.js
@@ -1,35 +1,38 @@
-export class Boss{
-    constructor(nom,attaque,vie) {
+export class Boss {
+    constructor(nom, attaque, vie) {
         this.nom = nom;
-        this.attaque = attaque
+        this.attaque = attaque;
         this.vie = vie;
         this.vieMax = vie;
+        this.element = this.getRandomElement(); // Randomly assign an element
 
         this.enigmes = [
             { question: "Quel est le plus grand océan du monde ?", reponse: "Pacifique" },
             { question: "Quelle est la capitale de la France ?", reponse: "Paris" },
             { question: "Quel est le plus grand mammifère terrestre ?", reponse: "Éléphant" }
         ];
+    }
 
-    this.poserEnigme =() => {
+    getRandomElement() {
+        const elements = ['Feu', 'Terre', 'Eau'];
+        return elements[Math.floor(Math.random() * elements.length)];
+    }
+
+    poserEnigme() {
         let enigme = this.enigmes[Math.floor(Math.random() * this.enigmes.length)];
         return enigme;
-t
+    }
 
-
-    }        
-        this.subir = (degats) => {
-            if (this.vie - degats > 0){
-                this.vie -= degats;
-
-            } else {
-                alert("Monstre mort");
-            }
+    subir(degats) {
+        if (this.vie - degats > 0) {
+            this.vie -= degats;
+        } else {
+            this.vie = 0;
+            alert("Monstre mort");
         }
-        this.attaquer =(cible) => {
-            cible.subir(this.attaque);
-        }
+    }
 
-        
+    attaquer(cible) {
+        cible.subir(this.attaque);
     }
 }
--- public/js/Mage.js
+++ public/js/Mage.js
@@ -1,27 +1,52 @@
+import { Héros } from './Héros.js';
 
-import {Héros} from './Héros.js'
-export class Mage extends Héros{
-    constructor(nom,posture,attaque,vie,mana) {
-        super(nom,posture,attaque,vie);
+// Function to randomly assign an element
+function getRandomElement() {
+    const elements = ['Feu', 'Terre', 'Eau'];
+    return elements[Math.floor(Math.random() * elements.length)];
+}
+
+export class Mage extends Héros {
+    constructor(nom, posture, attaque, vie, mana, element) {
+        super(nom, posture, attaque, vie);
         this.mana = mana;
-        this.subir = (degats)  => {
-            if (this.vie - degats > 0) {
-                this.vie -= degats;
-            } else {
-                this.vie = 0;
-                console.log(`${this.nom} est mort !`);
-            }
-        }
-        this.attaquer =(cible)  =>{
-            if (this.mana >= 2) {
-                this.mana -= 2;
-                console.log(`${this.nom} inflige ${this.attaque} points de dégâts à ${cible.nom}.`);
-                cible.vie -= this.attaque;
-            } else {
-                this.mana = 7;
-                console.log(`${this.nom} récupère 7 points de mana au lieu d'attaquer.`);
-            }
+        this.element = element; // Player chooses the element
+    }
+
+    subir(degats) {
+        if (this.vie - degats > 0) {
+            this.vie -= degats;
+        } else {
+            this.vie = 0;
+            console.log(`${this.nom} est mort !`);
         }
     }
-        
+
+    attaquer(cible) {
+        if (this.mana >= 2) {
+            this.mana -= 2;
+            let damage = this.attaque;
+
+            // Check for elemental dominance
+            if (this.elementDominates(cible.element)) {
+                damage *= 1.3; // 30% more damage
+                console.log(`${this.nom} utilise un sort élémentaire et inflige 30% de dégâts supplémentaires !`);
+            }
+
+            console.log(`${this.nom} inflige ${damage} points de dégâts à ${cible.nom}.`);
+            cible.vie -= damage;
+        } else {
+            this.mana = 7;
+            console.log(`${this.nom} récupère 7 points de mana au lieu d'attaquer.`);
+        }
     }
+
+    elementDominates(targetElement) {
+        const dominance = {
+            'Feu': 'Terre',
+            'Terre': 'Eau',
+            'Eau': 'Feu'
+        };
+        return dominance[this.element] === targetElement;
+    }
+}
